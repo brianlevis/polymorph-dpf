662752eda8275290ddf77c24c862146aea580cc6
Forest Hu <foresthu@berkeley.edu>
2018-03-31 16:44:48 -0700

    Added multishot.py - baseline for running multiple oneshot algorithms for different site ids

---
+++ oneshot/multishot.py
@@ -0,0 +1,25 @@
+import dpf
+from random import randint
+
+class MultiShot:
+    def __init__(self, num_shots, init_return=0.0, init_gap=.05):
+        self.num_shots = num_shots
+        self.oneshots = {init_return + init_gap*i: dpf.DynamicPriceFloor() for i in range(num_shots)}
+        self.ids = {} # id -> index of one shot algo
+
+    def get_price_floor(self, input_features):
+        site_id, num_bids = input_features['site_id'], len(input_features['bid_requests'])
+        if site_id not in self.ids:
+            # can also try using average return instead of random
+            oneshot = self.oneshots[randint(0, self.num_shots-1)]
+            pf = oneshot.get_price_floor(num_bids)
+            idx = pf//init_gap
+            self.ids[site_id] = idx
+        else:
+            pf = self.oneshots[self.ids[site_id]].get_price_floor(num_bids) 
+        self.pf = pf 
+        
+    def update(self, input_features):
+        bids = inpute_features['bid_requests']
+        if site_id in self.ids and hasattr(self, pf):
+            self.oneshots[self.ids[site_id]].update(bids, self.pf)
