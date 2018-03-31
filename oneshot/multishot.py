a3c9b79c4b9e4633044791a4d6a71ae6264fd463
Forest Hu <foresthu@berkeley.edu>
2018-03-31 16:48:58 -0700

    Fixed initializer and added some explaination for it

--- oneshot/multishot.py
+++ oneshot/multishot.py
@@ -2,9 +2,13 @@ import dpf
 from random import randint
 
 class MultiShot:
+    # oneshots[0] holds sites w/ returns from init_return to init_return+init_gap
+    # oneshots[1] similarly for init_return+init_gap to init_return+2*init_gap
     def __init__(self, num_shots, init_return=0.0, init_gap=.05):
         self.num_shots = num_shots
-        self.oneshots = {init_return + init_gap*i: dpf.DynamicPriceFloor() for i in range(num_shots)}
+        self.init_return = init_return
+        self.init_gap = init_gap
+        self.oneshots = [dpf.DynamicPriceFloor() for _ in range(num_shots)]
         self.ids = {} # id -> index of one shot algo
 
     def get_price_floor(self, input_features):
@@ -13,7 +17,7 @@ class MultiShot:
             # can also try using average return instead of random
             oneshot = self.oneshots[randint(0, self.num_shots-1)]
             pf = oneshot.get_price_floor(num_bids)
-            idx = pf//init_gap
+            idx = (pf+self.init_return)//self.init_gap
             self.ids[site_id] = idx
         else:
             pf = self.oneshots[self.ids[site_id]].get_price_floor(num_bids) 
