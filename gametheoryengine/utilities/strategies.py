46f4d008528a17ea8f50e885602cf314db48a629
Edric Xiang <edric.xiang@gmail.com>
2018-03-31 01:52:41 -0700

    Add WIP untested GameTheoryPerID strategy

--- gametheoryengine/utilities/strategies.py
+++ gametheoryengine/utilities/strategies.py
@@ -67,6 +67,87 @@ class GameTheory(engine.Strategy):
         self.second_sum -= second_bid
 
 
+class GameTheoryPerID(engine.Strategy):
+    """
+    Keeps a unique running average for each bidder. For each auction, 
+    participating bidders' running averages are averaged together, then
+    multiplied by self.fraction to get the reserve price. self.fraction should
+    act as a weight (i.e. 0.9 would cause the reserve to undershoot a bit)
+
+    @Hermish: b/c this needs bidder IDs attached to price, you'll need to add
+    functions to pass in extra info to params versus your GameTheory class.
+    In this case it's easier to just pass in bid_responses unprocessed since
+    I don't need to know first/second highest bidder per auction anymore.
+
+    """
+    def __init__(self, window_length, fraction):
+        """
+        :param window_length: (int) the number of previous auctions to store at
+            one time
+        :param fraction: [float] a decimal between 0 and 1 inclusive which
+            signifies the distance to the first bid
+        """
+        
+        self.fraction = fraction
+        self.window_length = window_length
+
+        # self.averages holds running avg (float) for each bidder ID key
+        # e.g. "12345": 0.000987. Bidder IDs are ints, may have to cast to
+        # str for them to be hashable, e.g. self.averages[str(bid['id'])].
+        self.averages = {}
+
+    def get_reserve(self, bid_responses):
+        """
+        (idk how you generate these, sorry bout the formatting)
+        bid_responses: the list of dicts from the JSON
+
+        Returns average of participating bidders' individual running avgs (if they
+        have bid before/exist in self.averages), multiplied by self.fraction to 
+        weight it (e.g. 0.9 to try to undershoot a bit).
+        """
+        if len(bid_responses) == 0:
+            return 0
+        
+        count = 0
+        reserve = 0
+        for bid in bid_responses:
+            if bid['id'] in self.averages:
+                reserve += self.averages[bid['bid_price']]
+                count += 1
+
+        if count == 0:
+            return 0 # Or return some constant STATIC_RESERVE
+
+        reserve = reserve / count
+
+        return self.fraction * reserve
+
+    def update(self, bid_responses):
+        """
+        bid_responses: the list of dicts from the JSON
+
+        Moving avg w/out tracking: https://stackoverflow.com/questions/12636613/
+        Using implementation from answer #2.
+        May want to consider exponential moving average in the future.
+        FLAW: earlier bids are weighted way too much until we start getting 
+        close to window_length number of bids in each bidder's running average.
+        """
+
+        if len(bid_responses) == 0:
+            return
+
+        for bid in bid_responses:
+            if bid['id'] in self.averages:
+                n = self.window_length
+                oldAvg = self.averages[bid['id']]
+                newBid = bid['bid_price']
+                # Below weighs early bids too much initially
+                newAvg = (oldAvg * (n - 1.0) / n) + (newBid / n)
+                self.averages[bid['id']] = newAvg
+            else:
+                self.averages[bid['id']] = bid['bid_price']
+
+
 class BruteForceOptimization(engine.Strategy):
     """
     An optimization approach that simply looks for the best price floor to
