c943c4fb65af36a021e445542191ee315b2aae6c
Nilay Khatore <khatorenilay@gmail.com>
2018-04-04 00:01:32 -0700

    Update both scripts

--- vwprediction/formatting_script.py
+++ vwprediction/formatting_script.py
@@ -11,23 +11,25 @@ line_iterator = utils.get_line_iterator((11, 0), (11, 0))
 
 text_file = open('formatted.txt', 'w+')
 
+def get_features(processed_line):
+    campaign_id, site_id, zone_id = 0, 0, 0
+    if 'site_id' in processed_line['input_features']:
+        site_id = processed_line['input_features']['site_id']
+    if 'zone_id' in processed_line['input_features']:
+        zone_id = processed_line['input_features']['zone_id']
+    if processed_line['bids']:
+        for br in processed_line['bid_responses']:
+            if br['bid_price'] == processed_line['bids'][0]:
+                campaign_id = br['id']
+    return (campaign_id, site_id, zone_id)
+
 while True:
     try:
-
         line_dict = next(line_iterator)
         processed_line = utils.prepare_line(line_dict)
-
-        campaign_id, site_id, zone_id = 0, 0, 0
-
-        if 'site_id' in processed_line['input_features']:
-            site_id = processed_line['input_features']['site_id']
-        if 'zone_id' in processed_line['input_features']:
-            zone_id = processed_line['input_features']['zone_id']
+        features = get_features(processed_line)
         if processed_line['bids']:
-            for br in processed_line['bid_responses']:
-                if br['bid_price'] == processed_line['bids'][0]:
-                    campaign_id = br['id']
-            text_file.write("{0} | campaign_id:{1} site_id:{2} zone_id:{3}\n".format(processed_line['bids'][0], campaign_id, site_id, zone_id))
+            text_file.write("{0} | campaign_id:{1} site_id:{2} zone_id:{3}\n".format(processed_line['bids'][0], features[0], features[1], features[2]))
     except StopIteration:
         break
 
--- vwprediction/simulate.py
+++ vwprediction/simulate.py
@@ -10,17 +10,38 @@ sim.utils.INPUT_FEATURES.add('campaign_id')
 
 class VWSimulator(sim.simulator.Simulator):
 
-    def calculate_price_floor(self, input_features):
+    def calculate_price_floor(self, input_features, prepared_line):
         #os.system("vw formatted.txt -p prediction.txt")
         #os.remove("prediction.txt")
+        campaign_id, site_id, zone_id = 0, 0, 0
+        if 'site_id' in input_features:
+            site_id = input_features['site_id']
+        if 'zone_id' in input_features:
+            zone_id = input_features['zone_id']
+        if prepared_line['bids']:
+            for br in prepared_line['bid_responses']:
+                if br['bid_price'] == prepared_line['bids'][0]:
+                    campaign_id = br['id']
         os.system("vw --daemon --port 26542 --quiet -i 5_passes.model -t --num_children 1")
-        os.system("echo '| campaign_id:{0} site_id:{1} zone_id:{2}' | nc localhost 26542".format(input_features['campaign_id'], input_features['site_id'], input_features['zone_id']))
+        os.system("echo '| campaign_id:{0} site_id:{1} zone_id:{2}' | nc localhost 26542".format(campaign_id, site_id, zone_id))
         os.system("pkill -9 -f 'vw.*--port 26542'")
         price_floor *= HYPER_PARAM
         return price_floor
 
     def process_line(self, line, input_features, bids):
         pass
+
+    def run_simulation(self, output='normal'):
+        line_iterator = get_line_iterator(start=self.start, stop=self.stop, limit=self.limit, download=self.download,
+                                          delete=self.delete)
+        for line in line_iterator:
+            prepared_line = prepare_line(line)
+            bids, input_features = prepared_line['bids'], prepared_line['input_features']
+            price_floor = self.calculate_price_floor(input_features, prepared_line)
+            self.stats.process_line(bids, input_features, price_floor)
+            self.process_line(line, input_features, bids)
+        if output != 'none':
+            self.stats.print_stats()
 
 def optimal_hyper_param():
     VWSim = VWSimulator((12, 0), (12, 0))
