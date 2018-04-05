f2a2e678a45a63f05db04f8683aa8003e55ef193
njaladan <ganesh.jaladanki@gmail.com>
2018-04-04 18:03:44 -0700

    got a really good revenue amount with this

--- oneshot/dpf.py
+++ oneshot/dpf.py
@@ -1,6 +1,8 @@
-class DynamicPriceFloor:
+from simulator import *
 
-    def __init__(self, price_floor=0.0002, eps=1.0, lamb_h=0.3, lamb_e=0.1, lamb_l=0.4, time=0, M=5):
+class OneShot():
+
+    def __init__(self, price_floor=0.0002, eps=1.0, lamb_h=0.1, lamb_e=0.46, lamb_l=0.1, time=0, M=5):
         self.price_floor = price_floor
         self.eps = eps
         self.lamb_h = lamb_h
@@ -39,15 +41,14 @@ class DynamicPriceFloor:
             return 0.0
         return price_floor - second
 
-    def get_price_floor(self, num_bids):
-        running_avg = sum(self.revenues)/len(self.revenues) if len(self.revenues) > 0 else 0
-        return self.price_floor if num_bids >= self.oneshot_min_n else running_avg
+    def calculate_price_floor(self, num_bids):
+        return self.price_floor
 
     def update(self, bids, price_floor):
         first, second = self.max2(bids)
         revenue = self.calculate_revenue(first, second, price_floor)
         diff = self.calculate_differential(first, second, price_floor)
-#        print(first, second, price_floor, revenue, diff, self.revenues)
+        #print(first, second, price_floor, revenue, diff, self.revenues)
         if len(bids) >= self.oneshot_min_n:
             self.oneshot(first, second)
         else:
@@ -58,4 +59,34 @@ class DynamicPriceFloor:
 
     def __str__(self):
         return  "price_floor: " + str(self.price_floor) + "\neps: " + str(self.eps) + "\nlamb_h: " + str(self.lamb_h) + "\nlamb_e: " + str(self.lamb_e) + "\nlamb_l: " + str(self.lamb_l) + "\ntime: " + str(self.time) + "\nM: " + str(self.M)
- 
+
+class OneShotSimulator(Simulator):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d = dict()
+
+
+    def calculate_price_floor(self, input_features):
+        if not input_features.get('ad_network_id', 0):
+            input_features['ad_network_id'] = 'None'
+        
+        if not self.d.get(input_features['ad_network_id'] ,0):
+            self.d[input_features['ad_network_id']] = OneShot()            
+
+        return self.d[input_features['ad_network_id']].calculate_price_floor(2)
+
+    def process_line(self, line, input_features, bids):
+        if not input_features.get('ad_network_id', 0):
+            input_features['ad_network_id'] = 'None'
+        
+        if not self.d.get(input_features['ad_network_id'] ,0):
+            self.d[input_features['ad_network_id']] = OneShot()
+            
+        pf = self.d[input_features['ad_network_id']].price_floor
+        self.d[input_features['ad_network_id']].update(bids, pf)
+            
+
+
+oneshot = OneShotSimulator(stop=(11, 0), limit=10, delete=False)
+oneshot.run_simulation()
