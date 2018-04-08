712d4dc3140ad6ca3a42bea5c1d638197875f898
Nilay Khatore <khatorenilay@gmail.com>
2018-04-08 16:48:08 -0700

    Change formatting to include input features, refactor simulator, plot of results on new data

--- vwprediction/formatting_script.py
+++ vwprediction/formatting_script.py
@@ -1,42 +1,34 @@
 import boto3
-
 import sys
 import os
 sys.path.append(os.path.abspath('../simulator'))
 import utils
 
 START = (12, 0)
-END = (12, 2)
-
-utils.INPUT_FEATURES.add('campaign_id')
+END = (12, 0)
 
 FILE_NAME = '{0}{1}to{2}{3}.txt'.format(str(START[0]), str(START[1]), str(END[0]), str(END[1]))
 
-line_iterator = utils.get_line_iterator(START, END)
+line_iterator = utils.get_line_iterator(START, END, limit=32)
 
 text_file = open(FILE_NAME, 'w+')
-
-def get_features(processed_line):
-    campaign_id, site_id, zone_id = 0, 0, 0
-    if 'site_id' in processed_line['input_features']:
-        site_id = processed_line['input_features']['site_id']
-    if 'zone_id' in processed_line['input_features']:
-        zone_id = processed_line['input_features']['zone_id']
-    if processed_line['bids']:
-        for br in processed_line['bid_responses']:
-            if br['bid_price'] == processed_line['bids'][0]:
-                campaign_id = br['id']
-    else:
-        campaign_id = processed_line['input_features']['campaign_id']
-    return (campaign_id, site_id, zone_id)
 
 while True:
     try:
         line_dict = next(line_iterator)
         processed_line = utils.prepare_line(line_dict)
-        features = get_features(processed_line)
         if processed_line['bids']:
-            text_file.write("{0} | campaign_id:{1} site_id:{2} zone_id:{3}\n".format(processed_line['bids'][0], features[0], features[1], features[2]))
+            line = str(processed_line['bids'][0]) + ' | '
+            for feature in processed_line['input_features']:
+                if str(feature) == 'bid_requests':
+                    for request_id in processed_line['input_features']['bid_requests']:
+                        line += 'campaign_id' + str(request_id) + ' '
+                elif str(feature) == 'r_timestamp':
+                    line += 'r_timestamp' + processed_line['input_features'][str(feature)].replace(':', '-') + ' '
+                else:
+                    line += str(feature) + str(processed_line['input_features'][str(feature)]) + ' '
+            line = line[:-1] + '\n'
+            text_file.write(line)
     except StopIteration:
         break
 
@@ -48,4 +40,4 @@ output_bucket.upload_file(Filename=FILE_NAME, Key=FILE_NAME)
 
 os.system('vw {0} --holdout_off -f {1}.model'.format(FILE_NAME, FILE_NAME[:-4]))
 
-#os.remove(FILE_NAME)
+os.remove(FILE_NAME)
--- vwprediction/simulate.py
+++ vwprediction/simulate.py
@@ -1,93 +1,65 @@
 import sys
 import os
-
 import socket as sock
-import time
 
 sys.path.append(os.path.abspath('..'))
 import simulator as sim
 
-sim.utils.INPUT_FEATURES.add('campaign_id')
-
 class VWSimulator(sim.simulator.Simulator):
 
-    def calculate_price_floor(self, input_features, prepared_line, line_num, socket):
-
-        campaign_id, site_id, zone_id = 0, 0, 0
-
-        if 'site_id' in input_features:
-            site_id = input_features['site_id']
-        if 'zone_id' in input_features:
-            zone_id = input_features['zone_id']
-        if prepared_line['bids']:
-            for br in prepared_line['bid_responses']:
-                if br['bid_price'] == prepared_line['bids'][0]:
-                    campaign_id = br['id']
-        else:
-            campaign_id = input_features['campaign_id']
-
-        try:
-            socket.sendall(bytes('| campaign_id:{0} site_id:{1} zone_id:{2}\n'.format(campaign_id, site_id, zone_id), 'utf-8'))
-            prediction = socket.recv(1024)
-            prediction = prediction[:-1].decode('utf-8')
-            return float(prediction)
-        except OSError as ose:
-            print(ose)
-            return None
-
-    def process_line(self, line, input_features, bids):
-        pass
-
-    def run_simulation(self, multiplier, socket, output='normal'):
-        line_iterator = sim.utils.get_line_iterator(start=self.start, stop=self.stop, limit=self.limit, download=self.download,
-                                          delete=self.delete)
-        line_num = 0
-        for line in line_iterator:
-            line_num += 1
-            if line_num % 2 == 0:
-                prepared_line = sim.utils.prepare_line(line)
-                bids, input_features = prepared_line['bids'], prepared_line['input_features']
-                price_floor = self.calculate_price_floor(input_features, prepared_line, line_num, socket)
-                if price_floor == None:
-                    break
-                price_floor *= multiplier
-                self.stats.process_line(bids, input_features, price_floor)
-        if output != 'none':
-            print('Multiplier:' + str(multiplier))
-            self.stats.print_stats()
-
-class DefaultSimulator(sim.simulator.Simulator):
-    """Represents a static, universal price floor. Note that Polymorph has a default floor of $0.10 CPM,
-    but not a universal floor. """
+    def __init__(self, start, end, socket, multiplier, **kwargs):
+        super().__init__(start, end, **kwargs)
+        self.socket = socket
+        self.multiplier = multiplier
 
     def calculate_price_floor(self, input_features):
-        return sim.simulator.DEFAULT_FLOOR
+        try:
+            line = '| '
+
+            for feature in input_features:
+                if str(feature) == 'bid_requests':
+                    for request_id in input_features['bid_requests']:
+                        line += 'campaign_id' + str(request_id) + ' '
+                elif str(feature) == 'r_timestamp':
+                    line += 'r_timestamp' + input_features[str(feature)].replace(':', '-') + ' '
+                else:
+                    line += str(feature) + str(input_features[str(feature)]) + ' '
+
+            line = line[:-1] + '\n'
+            self.socket.sendall(bytes(line, 'utf-8'))
+            prediction = self.socket.recv(1024)
+            prediction = prediction[:-1].decode('utf-8')
+            return float(prediction) * self.multiplier
+        except OSError as ose:
+            print(ose)
+            return 0.0001
 
     def process_line(self, line, input_features, bids):
         pass
 
 def optimal_multiplier(start, end, min_mult, max_mult, increment):
-    revenues = []
+    revenues = {}
     mult = min_mult
-    file_name = '{0}{1}to{2}{3}'.format(str(start[0] + 1), str(start[1]), str(end[0] + 1), '2')
-    while mult <= max_mult:
+    file_name = '{0}{1}to{2}{3}'.format(str(start[0] + 1), str(start[1]), str(end[0] + 1), str(end[1]))
 
-        VWSim = VWSimulator(start, end)
+    while mult <= max_mult:
 
         os.system("pkill -9 -f 'vw.*--port 12345'")
         os.system("vw --daemon --port 12345 --quiet -i {0}.model -t --num_children 1".format(file_name))
         socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
         socket.connect(('localhost', 12345))
 
-        VWSim.run_simulation(mult, socket)
-        revenues.append(VWSim.stats.total_revenue)
+        VWSim = VWSimulator(start, end, socket, mult, limit=32)
+        VWSim.run_simulation()
+        revenues[mult] = VWSim.stats.total_revenue
 
-        socket.close()
+        VWSim.socket.close()
         os.system("pkill -9 -f 'vw.*--port 12345'")
 
         mult += increment
+
     print(revenues)
     optimum = max(revenues)
     return (optimum, min_mult + increment * revenues.index(optimum))
 
-print(optimal_multiplier((11, 0), (11, 1), 0.2, 5, 0.2))
+print(optimal_multiplier((11, 0), (11, 0), 0.1, 0.9, 0.2))
