fc5b734aa92a1d76a57cec44cc9657afcc5b571b
unknown <edric.xiang@gmail.com>
2018-04-11 23:29:07 -0700

    Rename strats and implement generalized single id strat

--- gametheory/gametheory.py
+++ gametheory/gametheory.py
@@ -5,7 +5,7 @@ import simulator.simulator as sim
 
 DEFAULT_FLOOR = 0.1 / 1000
 
-class GameTheory(sim.Simulator):
+class Average(sim.Simulator):
     """
     Currently uses an *approximate* running average that doesn't require
     tracking past elements.
@@ -29,13 +29,12 @@ class GameTheory(sim.Simulator):
         self.highAvg = self.highAvg * (self.window - 1)/self.window +\
                 bids[1] * 1.0/self.window
 
-class GameTheoryPerID(sim.Simulator):
+class AverageBidderID(sim.Simulator):
     """
-    Currently uses a running average that doesn't require tracking past 
-    elements.
+    Uses a running average that doesn't require tracking past elements.
     Reserve is the greatest recorded average among ids from bid_requests, 
     multiplied by a weight parameter. *NOTE* weight parameter doesn't function
-    in the same way as the weight parameter from GameTheory().
+    in the same way as the weight parameter from Average().
     """
 
     def __init__(self, window, weight, start=(11, 0), stop=(15, 23), **kwargs):
@@ -56,6 +55,8 @@ class GameTheoryPerID(sim.Simulator):
             return max([self.averages[key] for key in seenIDs]) * self.weight
 
     def process_line(self, line, input_features, bids):
+        if len(bids) == 0:
+            return
         bidResponses = line["bid_responses"]
         for response in bidResponses:
             bidderID = response["id"]
@@ -69,6 +70,42 @@ class GameTheoryPerID(sim.Simulator):
                 self.averages[bidderID] = price
                 self.counts[bidderID] = 1
 
+class AverageSingleID(sim.Simulator):
+
+    """
+    Only works for input features with a single value (e.g. works with site_id, 
+    pub_network_id, but not bid_requests).
+    """
+
+    def __init__(self, window, weight, param_id, start=(11, 0), stop=(15, 23), **kwargs): 
+        Simulator.__init__(self, start, stop, **kwargs)
+        self.window = window
+        self.weight = weight
+        self.id = param_id
+        self.averages = {}
+        self.counts = {}
+
+    def calculate_price_floor(self, input_features):
+        key = input_features[self.id]
+        if key in self.averages:
+            return self.averages[key]
+        else: 
+            return DEFAULT_FLOOR
+
+    def process_line(self, line, input_features, bids):
+        if len(bids) < 2:
+            return
+        key = input_features[self.id]
+        low, high = bids[1], bids[0]
+        weightedAvg = low * self.weight + high * (1 - self.weight)
+        if key in self.averages:
+            n = min(self.counts[key] + 1, self.window)
+            self.averages[key] = self.averages[key] * (n - 1)/n +\
+                    weightedAvg * 1.0/n
+            self.counts[key] += 1
+        else:
+            self.averages[key] = weightedAvg
+            self.counts[key] = 1
 
 
 # {"geo_timezone": "America/Indianapolis", "pub_network_id": 267, "site_id":
