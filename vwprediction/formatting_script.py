b8786a30599f3b2d87c503075d97567bc372a7c5
Brian Levis <brianlevis1@gmail.com>
2018-04-16 12:36:22 -0700

    ready formatting script

--- vwprediction/formatting_script.py
+++ vwprediction/formatting_script.py
@@ -1,36 +1,38 @@
+import gzip
+
 import boto3
 import sys
 import os
 sys.path.append(os.path.abspath('..'))
 import simulator.utils as utils
 
-FILE_NAME = 'test.txt'
-
-text_file = open(FILE_NAME, 'w+')
-
-for line_dict in utils.get_line_iterator():
-    processed_line = utils.prepare_line(line_dict)
-    if processed_line['bids']:
-        line = str(processed_line['bids'][0]) + ' | '
-        for feature in processed_line['input_features']:
-            if str(feature) == 'bid_requests':
-                for request_id in processed_line['input_features']['bid_requests']:
-                    line += 'campaign_id' + str(request_id) + ' '
-            elif str(feature) == 'r_timestamp':
-                line += 'hour' + str(processed_line['input_features'][str(feature)])[11:13] + ' '
-            elif 'num_ads' in str(feature) or 'r_cnt' in str(feature):
-                line += feature + ':' + str(processed_line['input_features'][str(feature)]) + ' '
-            else:
-                line += feature + str(processed_line['input_features'][str(feature)]) + ' '
-        line = line[:-1] + '\n'
-        text_file.write(line)
-
-text_file.close()
+FILE_FORMAT = '%02d/part-%02d.vw.gz'
 
 output_bucket = boto3.resource('s3').Bucket('codebase-pm-vw-team')
 
-output_bucket.upload_file(Filename=FILE_NAME, Key=FILE_NAME)
+for d in range(11, 16):
+    for h in range(0, 24):
+        remote_filename = '%02d/part-%02d.vw.gz' % (d, h)
+        local_filename = remote_filename.replace('/', '_')
+        local_file = gzip.open(local_filename, 'wt')
+        for line_dict in utils.get_line_iterator(start=(d, h), stop=(d, h)):
+            processed_line = utils.prepare_line(line_dict)
+            if processed_line['bids']:
+                line = str(processed_line['bids'][0]) + ' | '
+                for feature in processed_line['input_features']:
+                    if feature == 'bid_requests':
+                        for request_id in processed_line['input_features']['bid_requests']:
+                            line += 'campaign_id' + str(request_id) + ' '
+                    elif feature == 'r_timestamp':
+                        line += 'hour' + str(processed_line['input_features'][feature])[11:13] + ' '
+                    elif 'num_ads' in feature or 'r_cnt' in feature:
+                        line += feature + ':' + str(processed_line['input_features'][feature]) + ' '
+                    else:
+                        line += feature + str(processed_line['input_features'][feature]) + ' '
+                line = line[:-1] + '\n'
+                local_file.write(line)
+        local_file.close()
+        output_bucket.upload_file(Filename=local_filename, Key=remote_filename)
+        os.remove(local_filename)
 
-os.system('vw {0} --holdout_off -f {1}.model'.format(FILE_NAME, FILE_NAME[:-4]))
-
-os.remove(FILE_NAME)
+# os.system('vw {0} --holdout_off -f {1}.model'.format(FILE_NAME, FILE_NAME[:-4]))
