17a65b7753dd00f7100d2f74f05b58f3a4709b79
Nilay Khatore <khatorenilay@gmail.com>
2018-04-15 19:23:51 -0700

    revert formatting

--- vwprediction/formatting_script.py
+++ vwprediction/formatting_script.py
@@ -7,7 +7,7 @@ import simulator.utils as utils
 START = (12, 0)
 END = (12, 0)
 
-FILE_NAME = '{0}{1}to{2}{3}.txt'.format(str(START[0]), str(START[1]), str(END[0]), str(END[1]))
+FILE_NAME = 'test.txt'#'{0}{1}to{2}{3}.txt'.format(str(START[0]), str(START[1]), str(END[0]), str(END[1]))
 
 line_iterator = utils.get_line_iterator(START, END, limit=32)
 
@@ -18,27 +18,18 @@ while True:
         line_dict = next(line_iterator)
         processed_line = utils.prepare_line(line_dict)
         if processed_line['bids']:
-            bid = str(processed_line['bids'][0]) + ' | '
-            geoline = '| '
-            ualine = '| '
-            numline = '| '
+            line = str(processed_line['bids'][0]) + ' | '
             for feature in processed_line['input_features']:
                 if str(feature) == 'bid_requests':
                     for request_id in processed_line['input_features']['bid_requests']:
-                        bid += 'campaign_id' + str(request_id) + ' '
+                        line += 'campaign_id' + str(request_id) + ' '
                 elif str(feature) == 'r_timestamp':
-                    bid += 'hour' + str(processed_line['input_features'][str(feature)])[11:13] + ' '
-                elif "geo" in str(feature):
-                    geoline += feature + str(processed_line['input_features'][str(feature)]) + ' '
-                elif "ua_" in str(feature):
-                    ualine += feature + str(processed_line['input_features'][str(feature)]) + ' '
-                elif "num_ads" in str(feature):
-                    numline += feature + ':' + str(processed_line['input_features'][str(feature)]) + ' '
-                elif "r_cnt" in str(feature):
-                    numline += feature + ':' + str(processed_line['input_features'][str(feature)]) + ' '
+                    line += 'hour' + str(processed_line['input_features'][str(feature)])[11:13] + ' '
+                elif 'num_ads' in str(feature) or 'r_cnt' in str(feature):
+                    line += feature + ':' + str(processed_line['input_features'][str(feature)]) + ' '
                 else:
-                    bid += feature + str(processed_line['input_features'][str(feature)]) + ' '
-            line = bid + geoline + ualine + numline[:-1] + '\n'
+                    line += feature + str(processed_line['input_features'][str(feature)]) + ' '
+            line = line[:-1] + '\n'
             text_file.write(line)
     except StopIteration:
         break
