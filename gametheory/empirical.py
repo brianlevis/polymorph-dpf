Fix sign error bug

gametheory/empirical.py
@@ -46,7 +46,6 @@ class LinearProgramming(simulator.Simulator):
             self.upper,
             bounds=self.bounds
         )
-
         if results.success:
             return results.x[0] * self.discount
         return self.default
@@ -83,10 +82,8 @@ class LinearProgramming(simulator.Simulator):
             of the optimization problem
         """
         dimension = 2 * size + 1
-        objective = np.zeros(dimension, dtype=int)
-        for pos in range(1, dimension, 2):
-            objective[pos] = 1
-            objective[pos + 1] = -1
+        objective = np.ones(dimension)
+        objective[0] = 0
         return objective
 
     @staticmethod
@@ -125,6 +122,6 @@ class LinearProgramming(simulator.Simulator):
 
 
 if __name__ == '__main__':
-    settings = [30, 0.000001, 1, 0.1 / 1000]
+    settings = [5, 0.000001, 1, 0.1 / 1000]
     test = LinearProgramming(*settings, limit=1, download=False, delete=False)
     test.run_simulation()