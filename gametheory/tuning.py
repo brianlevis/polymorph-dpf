f9781e45392207ca3f8046231d122bd8f6c70d94
unknown <edric.xiang@gmail.com>
2018-04-18 22:15:58 -0700

    Update to use Brian's parallel sim runner

--- gametheory/tuning.py
+++ gametheory/tuning.py
@@ -8,15 +8,18 @@ test_windows = [50, 100, 250, 500, 1000]
 test_weights = [x / 10.0 for x in range(0, 11, 1)]
 test_bidder_id_weights = [x / 10.0 for x in range(0, 11, 1)]
 
+default_start = (11, 0)
+default_stop = (11, 2)
+default_limit = 100
+
 def download():
     """
     Dummy run to download all files for the first time
-    Using default time frame: start=(11, 0), stop=(15, 23), limit=None
     """
-    download = Average(1, 0.5, download=True, delete=False)
+    download = Average(1, 0.5, start=default_start, stop=default_stop, limit=default_limit, download=True, delete=False)
     download.run_simulation()
 
-def tuneAverage(output, windows=test_windows, weights=test_weights):
+def tuneAverage(windows=test_windows, weights=test_weights):
     """
     :param windows: list of window values to test
     :param weights: list of weight values to test
@@ -24,13 +27,14 @@ def tuneAverage(output, windows=test_windows, weights=test_weights):
     """
     for window in windows:
         for weight in weights:
-            sim = Average(window, weight, download=False, delete=False)
-            sim.run_simulation()
-            stats = getStats(sim)
-            params = ("Average", window, weight)
-            output[params] = stats
+            sim = Average(window, weight, start=default_start, stop=default_stop, limit=default_limit, download=False, delete=False)
+            name = "Average : window={}, weight={}".format(window, weight)
+            queue_simulator(sim, name)
+    
+    output = run_queue(output='none')
+    return output
 
-def tuneAverageBidderID(output, windows=test_windows, weights=test_bidder_id_weights):
+def tuneAverageBidderID(windows=test_windows, weights=test_bidder_id_weights):
     """
     :param windows: list of window values to test
     :param weights: list of bidder weight values to test
@@ -40,13 +44,14 @@ def tuneAverageBidderID(output, windows=test_windows, weights=test_bidder_id_wei
     """
     for window in windows:
         for weight in weights:
-            sim = AverageBidderID(window, weight, download=False, delete=False)
-            sim.run_simulation()
-            stats = getStats(sim)
-            params = ("AverageBidderID", window, weight)
-            output[params] = stats 
+            sim = AverageBidderID(window, weight, start=default_start, stop=default_stop, limit=default_limit, download=False, delete=False)
+            name = "AverageBidderID : window={}, weight={}".format(window, weight)
+            queue_simulator(sim, name)
+    
+    output = run_queue(output='none')
+    return output
 
-def tuneAverageSingleID(output, windows=test_windows, weights=test_weights, param_id=None):
+def tuneAverageSingleID(windows=test_windows, weights=test_weights, param_id=None):
     """
     :param windows: list of window values to test
     :param weights: list of weight values to test
@@ -55,43 +60,44 @@ def tuneAverageSingleID(output, windows=test_windows, weights=test_weights, para
     """
     for window in windows:
         for weight in weights:
-            sim = AverageSingleID(window, weight, param_id, download=False, delete=False)
-            sim.run_simulation()
-            stats = getStats(sim)
-            params = ("AverageSingleID", window, weight, param_id)
-            output[params] = stats 
+            sim = AverageSingleID(window, weight, param_id, start=default_start, stop=default_stop, limit=default_limit, download=False, delete=False)
+            name = "AverageBidderID : param_id={}, window={}, weight={}".format(param_id, window, weight)
+            queue_simulator(sim, name)
+    
+    output = run_queue(output='none')
+    return output
 
-def printPrettyStats(stats):
-    # print("-------------------------------------------")
-    # print()
-    # print("-------------------------------------------")
-    for k in stats:
-        if k == "price_floor_hit_percent" or k == "price_floor_too_high_percent":
-            print("{}: {:.2%}".format(k, stats[k]))
-        else:
-            print("{}: {}".format(k, stats[k]))
+# def printPrettyStats(stats):
+#     # print("-------------------------------------------")
+#     # print()
+#     # print("-------------------------------------------")
+#     for k in stats:
+#         if k == "price_floor_hit_percent" or k == "price_floor_too_high_percent":
+#             print("{}: {:.2%}".format(k, stats[k]))
+#         else:
+#             print("{}: {}".format(k, stats[k]))
 
-def getStats(sim):
-    stats = {}
+# def getStats(sim):
+#     stats = {}
 
-    stats["timer"] = sim.timer
-    stats["total_revenue"] = sim.total_revenue
-    stats["auction_count"] = sim.auction_count
-    stats["auction_count_non_null"] = sim.auction_count_non_null
-    stats["price_floor_hit_count"] = sim.price_floor_hit_count
-    stats["price_floor_too_high_count"] = sim.price_floor_too_high_count
+#     stats["timer"] = sim.timer
+#     stats["total_revenue"] = sim.total_revenue
+#     stats["auction_count"] = sim.auction_count
+#     stats["auction_count_non_null"] = sim.auction_count_non_null
+#     stats["price_floor_hit_count"] = sim.price_floor_hit_count
+#     stats["price_floor_too_high_count"] = sim.price_floor_too_high_count
 
-    stats["price_floor_hit_percent"] = sim.price_floor_hit_count / sim.auction_count_non_null
-    stats["price_floor_too_high_percent"] = sim.price_floor_too_high_count / sim.auction_count_non_null
+#     stats["price_floor_hit_percent"] = sim.price_floor_hit_count / sim.auction_count_non_null
+#     stats["price_floor_too_high_percent"] = sim.price_floor_too_high_count / sim.auction_count_non_null
 
-    stats["average_revenue"] = sim.average_revenue
-    stats["average_revenue_non_null"] = sim.average_revenue_non_null
+#     stats["average_revenue"] = sim.average_revenue
+#     stats["average_revenue_non_null"] = sim.average_revenue_non_null
 
-    stats["total_bid_count"] = sim._total_bid_count
-    stats["total_bid_amount"] = sim._total_bid_amount
-    stats["average_bid_count"] = sim.average_bid_count
-    stats["average_bid_count_non_null"] = sim.average_bid_count_non_null
-    stats["average_bid_amount"] = sim.average_bid_amount
-    stats["average_bid_amount_non_null"] = sim.average_bid_amount_non_null
+#     stats["total_bid_count"] = sim._total_bid_count
+#     stats["total_bid_amount"] = sim._total_bid_amount
+#     stats["average_bid_count"] = sim.average_bid_count
+#     stats["average_bid_count_non_null"] = sim.average_bid_count_non_null
+#     stats["average_bid_amount"] = sim.average_bid_amount
+#     stats["average_bid_amount_non_null"] = sim.average_bid_amount_non_null
 
-    return stats
+#     return stats
