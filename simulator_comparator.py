5b02efedeaef19e9982ba7fd64ef9bf86b8deb90
Brian Levis <brianlevis1@gmail.com>
2018-04-24 15:24:12 -0700

    ready to test

--- simulator_comparator.py
+++ simulator_comparator.py
@@ -1,48 +1,55 @@
+from gametheory import AverageSimulateBid
 from simulator import *
-from multishot import MultiShot
-from gametheory.linear_programming import LinearProgramming
 
 
-class DefaultSimulator(Simulator):
-    """Represents a static, universal price floor. Note that Polymorph has a default floor of $0.10 CPM,
-    but not a universal floor. """
+class NoFloor(Simulator):
+    """Represents no price floor."""
 
     def calculate_price_floor(self, input_features):
-        return DEFAULT_FLOOR
+        return 0
 
     def process_line(self, line, input_features, bids):
         pass
 
 
-class NaiveSimulator(Simulator):
+class GlobalRunningAverage(Simulator):
     """Represents a simple running average floor."""
 
-    def __init__(self, *args, history_len=5, **kwargs):
+    def __init__(self, *args, history_len=25, weight=0.45, **kwargs):
         super().__init__(*args, **kwargs)
         from collections import deque
         self.most_recent_bids = deque([0] * history_len, maxlen=history_len)
+        self.weight = weight
 
     def calculate_price_floor(self, input_features):
-        return 0.75 * sum(self.most_recent_bids) / self.most_recent_bids.maxlen
+        return self.weight * sum(self.most_recent_bids) / self.most_recent_bids.maxlen
 
     def process_line(self, line, input_features, bids):
         if len(bids) > 0:
             self.most_recent_bids.append(bids[0])
 
 
+# tune running average
+# for h in range(15, 100, 10):
+#     for w in [0.35, 0.375, 0.4, 0.425, 0.45]:
+#         queue_simulator(GlobalRunningAverage(history_len=h, weight=w), 'Avg: h=%d, w=%f' % (h, w))
+# run_queue(start=(13, 12), stop=(13, 12), limit=15)
+
 # default_simulator = DefaultSimulator(stop=(11, 23))
-naive_simulator_100 = NaiveSimulator(history_len=100)
-naive_simulator_200 = NaiveSimulator(history_len=200)
-oneshot_simulator = MultiShot(10)
-lp_simulator_5 = LinearProgramming(5, 0.000001, 1, 0.1 / 1000)
-lp_simulator_10 = LinearProgramming(10, 0.000001, 1, 0.1 / 1000)
-
-queue_simulator(naive_simulator_100, 'Naive-100')
-queue_simulator(naive_simulator_200, 'Naive-200')
-queue_simulator(oneshot_simulator, 'OneShot')
-queue_simulator(lp_simulator_5, 'LP-5')
-
-run_queue(stop=(11, 0), limit=2)
+# naive_simulator_100 = NaiveSimulator(history_len=100)
+# naive_simulator_200 = NaiveSimulator(history_len=200)
+# oneshot_simulator = MultiShot(10)
+# lp_simulator_5 = LinearProgramming(5, 0.000001, 1, 0.1 / 1000)
+# lp_simulator_10 = LinearProgramming(10, 0.000001, 1, 0.1 / 1000)
+#
+# queue_simulator(naive_simulator_100, 'Naive-100')
+# queue_simulator(naive_simulator_200, 'Naive-200')
+# queue_simulator(oneshot_simulator, 'OneShot')
+# queue_simulator(lp_simulator_5, 'LP-5')
+queue_simulator(AverageSimulateBid(500, 0.6, 0.3), 'GT (High Revenue)')
+queue_simulator(AverageSimulateBid(500, 0.8, 0.2), 'GT (Low Overshoot)')
+queue_simulator(GlobalRunningAverage(), 'Basic Running Average')
+results = run_queue()
 
 
 # oneshot_simulator_site = MultiShot(id='site_id', stop=(11, 23))
