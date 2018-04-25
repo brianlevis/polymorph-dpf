5e994788202ab0faeb9c410a0f8c6a62d607b5d1
Nilay Khatore <khatorenilay@gmail.com>
2018-04-24 18:06:23 -0700

    parallelize testing script

--- vwprediction/testing.py
+++ vwprediction/testing.py
@@ -1,8 +1,25 @@
+import sys
+import os
+import socket as sock
+from random import randint
 from simulate import *
+sys.path.append('../')
+from simulator import *
 
-'''Testing using 3-day models on 2 hours of data from 2/14.'''
-all_revenues = {}
+
+'''Testing using all 5 3-day models on 2 hours of data from 2/14.'''
+socket_nums = []
 for p in range(1, 6):
     model_name = 'models/%01d_pass' % p
-    all_revenues[p] = optimal_multiplier((14, 0), (14, 0), 0.5, 1.5, 0.1, model_name)
-print(all_revenues)
+    for m in range(5, 16):
+        multiplier = m / 10
+        socket_num = randint(10000, 65000)
+        socket_nums.append(socket_num)
+        os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
+        os.system("vw --daemon --port {0} --quiet -i {1}.model -t --num_children 1".format(socket_num, model_name))
+        socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        socket.connect(('localhost', socket_num))
+        queue_simulator(VWSimulator((14, 0), (14, 1), socket, multiplier), 'VW (Model: {0}, Multiplier: {1})'.format(p, multiplier))
+results = run_queue()
+for socket_num in socket_nums:
+    os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
