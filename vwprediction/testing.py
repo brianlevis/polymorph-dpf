c83eda78959093e1b27a65839cd7ef422f386699
Brian Levis <brianlevis1@gmail.com>
2018-04-25 14:10:31 -0700

    add model comparison test results

--- vwprediction/testing.py
+++ vwprediction/testing.py
@@ -2,13 +2,14 @@ import sys
 import socket as sock
 from random import randint
 
-from vwprediction.simulate import VWSimulator
+from simulate import VWSimulator
 
 sys.path.append('../')
 from simulator import *
 
 
 '''Testing using all 5 3-day models on 2 hours of data from 2/14.'''
+# Edit: running on 29 minutes or so due to time
 socket_nums = []
 for p in range(1, 6):
     model_name = 'models/%01d_pass' % p
@@ -21,6 +22,6 @@ for p in range(1, 6):
         socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
         socket.connect(('localhost', socket_num))
         queue_simulator(VWSimulator(socket, multiplier), 'VW (Model: {0}, Multiplier: {1})'.format(p, multiplier))
-results = run_queue(start=(14, 0), stop=(14, 1))
+results = run_queue(start=(14, 0), stop=(14, 1), limit=15)
 for socket_num in socket_nums:
     os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
