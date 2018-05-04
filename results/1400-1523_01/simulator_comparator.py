77ba4f819141ed47043a1b6926f20e661e4967a6
Brian Levis <brianlevis1@gmail.com>
2018-05-03 18:01:22 -0700

    move testing file

--- results/1400-1523_01/simulator_comparator.py
+++ results/1400-1523_01/simulator_comparator.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from gametheory import AverageSingleID
 from oneshot import MultiShot
 from vwprediction.simulate import VWSimulator
@@ -5,17 +7,25 @@ from simulator import *
 
 import socket as sock
 
-class NoFloor(Simulator):
-    """Represents no price floor."""
+
+class RunningAverageWithZeros(Simulator):
+    """Represents a simple running average floor."""
+
+    def __init__(self, *args, history_len=150, weight=10.0, **kwargs):
+        super().__init__(*args, **kwargs)
+        from collections import deque
+        self.most_recent_bids = deque([0] * history_len, maxlen=history_len)
+        self.weight = weight
 
     def calculate_price_floor(self, input_features):
-        return 0
+        return self.weight * sum(self.most_recent_bids) / self.most_recent_bids.maxlen
 
     def process_line(self, line, input_features, bids):
-        pass
+        bid = 0.0 if len(bids) == 0 else bids[0]
+        self.most_recent_bids.append(bid)
 
 
-class GlobalRunningAverage(Simulator):
+class RunningAverageWithoutZeros(Simulator):
     """Represents a simple running average floor."""
 
     def __init__(self, *args, history_len=25, weight=0.45, **kwargs):
@@ -30,33 +40,36 @@ class GlobalRunningAverage(Simulator):
     def process_line(self, line, input_features, bids):
         if len(bids) > 0:
             self.most_recent_bids.append(bids[0])
+
 
 class RunningAverageWithBuckets(Simulator):
     """Represents a simple running average floor."""
 
-    buckets = {}
-
-    def __init__(self, *args, history_len=25, weight=0.45, **kwargs):
+    def __init__(self, *args, history_len=25, weight=0.45, feature='site_id', **kwargs):
         super().__init__(*args, **kwargs)
-        from collections import deque
-        self.most_recent_bids = deque([0] * history_len, maxlen=history_len)
+        self.history_len = history_len
+        self.feature = feature
         self.weight = weight
+        self.history = {}
 
     def calculate_price_floor(self, input_features):
-        return self.weight * sum(self.most_recent_bids) / self.most_recent_bids.maxlen
+        if input_features[self.feature] not in self.history:
+            self.history[input_features[self.feature]] = deque([0], maxlen=self.history_len)
+        hist = self.history[input_features[self.feature]]
+        return self.weight * sum(hist) / hist.maxlen
 
     def process_line(self, line, input_features, bids):
-        if len(bids) > 0:
-            self.most_recent_bids.append(bids[0])
+        bid = 0.0 if len(bids) == 0 else bids[0]
+        self.history[input_features[self.feature]].append(bid)
 
 # Run comparison
 
 
 # --- Tune running average ---
-# for h in range(15, 100, 10):
-#     for w in [0.35, 0.375, 0.4, 0.425, 0.45]:
+# for h in [150, 160]:
+#     for w in range(8, 20, 4):
 #         queue_simulator(GlobalRunningAverage(history_len=h, weight=w), 'Avg: h=%d, w=%f' % (h, w))
-# run_queue(start=(13, 12), stop=(13, 12), limit=15)
+# run_queue(start=(13, 12), stop=(13, 12), limit=5)
 
 # default_simulator = DefaultSimulator(stop=(11, 23))
 # naive_simulator_100 = NaiveSimulator(history_len=100)
@@ -75,31 +88,21 @@ class RunningAverageWithBuckets(Simulator):
 queue_simulator(AverageSingleID(500, 0.6, 0.3, "site_id"), 'GT (High Revenue)')
 
 oneshot_args = {'price_floor': 0.0002, 'eps': 1.0, 'lamb_h': 0.02, 'lamb_e': 0.9, 'lamb_l': 0.88, 'time': 0, 'M': 5}
-queue_simulator(MultiShot(84, oneshot_args=oneshot_args), 'MultiShot')
+queue_simulator(MultiShot(84, oneshot_args=oneshot_args), 'MultiShot with 84 Buckets')
 
-# socket_num = 12345
-# model_name = '~/polymorph-dpf/vwprediction/models/1_pass'
-# multiplier = 3.0
-# os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
-# os.system("~/vowpal_wabbit/vowpalwabbit/vw --daemon --port {0} --quiet -i {1}.model -t --num_children 1".format(socket_num, model_name))
-# socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-# socket.connect(('localhost', socket_num))
-# queue_simulator(VWSimulator(socket, multiplier), 'VW (Multiplier: {0})'.format(multiplier))
+socket_num = 12345
+model_name = '~/polymorph-dpf/vwprediction/models/1_pass'
+multiplier = 3.0
+os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
+os.system("~/vowpal_wabbit/vowpalwabbit/vw --daemon --port {0} --quiet -i {1}.model -t --num_children 1".format(socket_num, model_name))
+socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+socket.connect(('localhost', socket_num))
+queue_simulator(VWSimulator(socket, multiplier), 'VW (Multiplier: {0})'.format(multiplier))
 
-queue_simulator(GlobalRunningAverage(), 'Basic Running Average')
-# queue_simulator(NoFloor(), 'No Price Floor')
+queue_simulator(RunningAverageWithoutZeros(), 'Basic Running Average of Top Bids')
+# queue_simulator(RunningAverageWithBuckets(), 'Running Average by site_id')
+# queue_simulator(RunningAverageWithBuckets(feature='pub_network_id'), 'Running Average by pub_network_id')
 
 results = run_queue(start=(14, 0))
 
-# os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
-
-# --- Test OneShot Bucket distribution
-# num_buckets = 20
-# oneshot_args = {'price_floor': 0.0002, 'eps': 1.0, 'lamb_h': 0.1, 'lamb_e': 0.1, 'lamb_l': 0.1, 'time': 0, 'M': 5}
-# queue_simulator(MultiShot(num_buckets, oneshot_args=oneshot_args), 'Multishot bucket test')
-# results = run_queue(limi t=1, delete=False)
-# totals = {str(n): 0 for n in range(num_buckets)}
-# ids = results[0][1].ids
-# for b in ids.values():
-#     totals[str(b)] += 1
-# print(totals)
+os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
