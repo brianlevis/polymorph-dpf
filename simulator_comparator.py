463671b6405ce7d886ad24a1d20f506c2c62b97d
Brian Levis <brianlevis1@gmail.com>
2018-05-04 09:52:28 -0700

    cleanup test file

--- simulator_comparator.py
+++ simulator_comparator.py
@@ -1,50 +1,37 @@
+import socket
+
+from gametheory import AverageSingleID
+from oneshot import MultiShot
 from running_average.running_average import *
-
-# Run comparison
-
-
-# # --- Tune running average ---
-# for w in range(8, 11, 1):
-#     for h in range(5, 25, 5):
-#         queue_simulator(BucketedRunningAverageWithZeros(history_len=h, weight=w/10.0), 'Bucketed Without Average Avg: h=%d, w=%f' % (h, w/10.0))
-# run_queue(limit=2)
-
-#6:58 00-07
-queue_simulator(RunningAverageWithoutZeros(), "Running Average Without Zeros")
-queue_simulator(RunningAverageWithZeros(), "Running Average With Zeros")
-queue_simulator(BucketedRunningAverageWithoutZeros(), "Bucketed Running Average Without Zeros")
-queue_simulator(BucketedRunningAverageWithZeros(), "Bucketed Running Average With Zeros")
-results = run_queue(start=(14, 0), download=False, delete=False)
-
-
-# default_simulator = DefaultSimulator(stop=(11, 23))
-# naive_simulator_100 = NaiveSimulator(history_len=100)
-# naive_simulator_200 = NaiveSimulator(history_len=200)
-# oneshot_simulator = MultiShot(10)
-# lp_simulator_5 = LinearProgramming(5, 0.000001, 1, 0.1 / 1000)
-# lp_simulator_10 = LinearProgramming(10, 0.000001, 1, 0.1 / 1000)
 
 
 # --- Test All ---
 # Running average
-# queue_simulator(AverageSingleID(500, 0.6, 0.3, "site_id"), 'GT (High Revenue)')
-#
-# oneshot_args = {'price_floor': 0.0002, 'eps': 1.0, 'lamb_h': 0.02, 'lamb_e': 0.9, 'lamb_l': 0.88, 'time': 0, 'M': 5}
-# queue_simulator(MultiShot(84, oneshot_args=oneshot_args), 'MultiShot with 84 Buckets')
-#
-# socket_num = 12345
-# model_name = '~/polymorph-dpf/vwprediction/models/1_pass'
-# multiplier = 3.0
-# os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
-# os.system("~/vowpal_wabbit/vowpalwabbit/vw --daemon --port {0} --quiet -i {1}.model -t --num_children 1".format(socket_num, model_name))
-# socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-# socket.connect(('localhost', socket_num))
-# queue_simulator(VWSimulator(socket, multiplier), 'VW (Multiplier: {0})'.format(multiplier))
-#
-# queue_simulator(RunningAverageWithoutZeros(), 'Basic Running Average of Top Bids')
-# # queue_simulator(RunningAverageWithBuckets(), 'Running Average by site_id')
-# # queue_simulator(RunningAverageWithBuckets(feature='pub_network_id'), 'Running Average by pub_network_id')
-#
-# results = run_queue(start=(14, 0))
-#
-# os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
+from vwprediction.simulate import VWSimulator
+
+queue_simulator(RunningAverageWithoutZeros(), "Running Average Without Zeros")
+queue_simulator(RunningAverageWithZeros(), "Running Average With Zeros")
+queue_simulator(BucketedRunningAverageWithoutZeros(), "Bucketed Running Average Without Zeros")
+queue_simulator(BucketedRunningAverageWithZeros(), "Bucketed Running Average With Zeros")
+
+# Weighted Running average
+queue_simulator(AverageSingleID(500, 0.6, 0.3, "site_id"), 'GT (High Revenue)')
+
+# OneShot
+oneshot_args = {'price_floor': 0.0002, 'eps': 1.0, 'lamb_h': 0.02, 'lamb_e': 0.9, 'lamb_l': 0.88, 'time': 0, 'M': 5}
+queue_simulator(MultiShot(84, oneshot_args=oneshot_args), 'MultiShot with 84 Buckets')
+
+# Vowpal Wabbit
+socket_num = 12345
+model_name = '~/polymorph-dpf/vwprediction/models/1_pass'
+multiplier = 3.0
+os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
+os.system("~/vowpal_wabbit/vowpalwabbit/vw --daemon --port {0} --quiet -i {1}.model -t --num_children 1".format(socket_num, model_name))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', socket_num))
+queue_simulator(VWSimulator(sock, multiplier), 'VW (Multiplier: {0})'.format(multiplier))
+
+# Run all in parallell
+results = run_queue(start=(14, 0), limit=5)
+
+os.system("pkill -9 -f 'vw.*--port {0}'".format(socket_num))
